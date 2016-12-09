a0cb8d52fbfa91adbb1cec276c9d658612a442d2
alieissa <ali.portsudan@gmail.com>
2016-12-09 07:43:05 -0500

    Refactored out parsing and split install into 2 funcs.

--- apm/install.js
+++ apm/install.js
@@ -1,78 +1,49 @@
+'use strict';
+
 let shelljs = require('shelljs');
+
+let parseNPMOutput = require('./parse.js').parseNPMOutput;
+let parseSaveFlags = require('./parse.js').parseSaveFlags;
+
 let sortObject = require('./util.js').sortObject;
 
 // After installation adds packs to group list
 // if no group, creates new one
-function addToGroup(packs, groupDeps = {}) {
-
-    let groupDeps_ = JSON.parse(JSON.stringify(groupDeps));
-    packs.map(pack => installPack(pack))
-        .filter(result => result !== 1)
-        .map(npmOutput => parseNPMOutput(npmOutput))
-        .forEach(pack => {
-            groupDeps_[pack.name] = pack.version;
-        });
-    return sortObject(groupDeps_);
-}
-
-// Install NPM package.
 function installPack(pack) {
-    let output = shelljs.exec(`npm install ${pack}`, {silent:true})
-    return output.code === 0 ? output.stdout : output.code;
+
+    let _output = shelljs.exec(`npm install ${pack}`);
+    let _packInfo = parseNPMOutput(_output.stdout);
+
+    return {code: _output.code, name: _packInfo.name, version: _packInfo.version};
 }
 
+function installPacks(packs, groupNames, groupPacks) {
 
-// Get the installed pack name and version from NPM output
-function parseNPMOutput(npmOutput) {
-
-    let [_pack] = npmOutput.split(' ').slice(1)
-        .filter(substr => substr.indexOf('@') !== -1);
-
-    let name = _pack.split('@')[0];
-    let version = '^' + _pack.split('@')[1];
-    return {name, version};
-}
-
-// Separates the non dependecy related info from dep related info
-function parsePackageJson(packJson) {
-
-    let  packageJson_ = {};
-    let installedDeps_ = {};
-    
-    Object.keys(packJson).forEach(key => {
-        if(key.indexOf('Dependencies') === -1) {
-            packageJson_[key] = packJson[key]
+    let _verify = installResult => installResult.code === 0;
+    let _update = (output, group) => {
+        try {
+            groupPacks[group][output.name] = output.version;
         }
-        else {
-            installedDeps_[key] = packJson[key]
+        catch(TypeError) {
+            groupPacks[group] = {[output.name]: output.version};
         }
+
+    };
+
+    groupNames.forEach(group => {
+
+        // Install package, verify installation, update group packs if install good
+        packs.map(installPack).filter(_verify).forEach(output => _update(output, group));
+        groupPacks[group] = sortObject(groupPacks[group]);
     });
-    return [installedDeps_, packageJson_];
+
+    return sortObject(groupPacks);
 }
 
-//Returns sorted group array from flags that are in format --save-group
-function parseSaveFlags(flags) {
+function installGroup(groupName, groups) {
 
-    return flags.filter(flag => flag.indexOf('save-') === 0)
-        .map(flag => flag.split('save-')[1])
-        .map(group => `${group}Dependencies`)
-        .sort();
+    let _deps = groups[groupName];
+    Object.keys(_deps).forEach(installPack);
 }
 
-function install(packs, options, flags,  packJson) {
-
-    let depGroup = options['only'];
-    let installationGroups = parseSaveFlags(flags);
-    let [_installedDeps, _packageJson_] = parsePackageJson(packJson);
-
-    if(packs.length > 0 && installationGroups.length > 0) {
-
-        installationGroups.forEach(group => {
-            _installedDeps[group] = addToGroup(packs, _installedDeps[group]);
-        });
-        _installedDeps = sortObject(_installedDeps);
-        return [sortObject(_installedDeps), _packageJson_];
-    }
-}
-
-module.exports = install;
+module.exports = {packs: installPacks, group: installGroup};
