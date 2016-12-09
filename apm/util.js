c186252a6791c3a713acdf88abc8bf5fa8d0631f
alieissa <ali.portsudan@gmail.com>
2016-12-09 11:25:45 -0500

    Added func to exclude key from object

--- apm/util.js
+++ apm/util.js
@@ -9,4 +9,26 @@ function sortObject(object) {
     return object_;
 };
 
-module.exports = {sortObject};
+function exclude(object, key) {
+
+    let test = Object.keys(object).reduce((_obj, _key) => {
+        if(_key !== key) {
+            _obj[_key] = object[_key];
+        }
+        return _obj;
+    }, {});
+    // console.log(test);
+
+    let _object = {};
+
+    Object.keys(object).filter(_key =>  _key !== key).map(_key => {
+        _object[_key] = object[_key]
+    });
+
+    return _object;
+}
+
+function isEmpty(object) {
+    return Object.keys(object).length < 1;
+}
+module.exports = {sort: sortObject, exclude: exclude, isEmpty: isEmpty};
